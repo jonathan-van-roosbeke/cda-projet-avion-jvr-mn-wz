45cb23a8806d70919f87afdfde5ab9f9cff03987
Van Roosbeke <jonathan.van.roosbeke@gmail.com>
2020-06-16 14:24:59 +0200

    Ajout Sprite 2D flèches et vie

--- src/com/avion/constante/AnimatedPictures.java
+++ src/com/avion/constante/AnimatedPictures.java
@@ -2,15 +2,15 @@ package com.avion.constante;
 
 public abstract class AnimatedPictures {
 
-	private static String[] tVaisseau = { "/resource/images/vaisseau/vaisseau_top.png",
-			"/resource/images/vaisseau/vaisseau_left.png", "/resource/images/vaisseau/vaisseau_right.png" };
+	private static String[] tVaisseau = { "/resources/images/vaisseau/vaisseau_top.png",
+			"/resources/images/vaisseau/vaisseau_left.png", "/resources/images/vaisseau/vaisseau_right.png" };
 
-	private static String[] tExplosion = { "/resource/images/explosion/explosion_01.png",
-			"/resource/images/explosion/explosion_02.png", "/resource/images/explosion/explosion_03.png",
-			"/resource/images/explosion/explosion_04.png", "/resource/images/explosion/explosion_05.png",
-			"/resource/images/explosion/explosion_06.png", "/resource/images/explosion/explosion_07.png",
-			"/resource/images/explosion/explosion_08.png", };
+	private static String[] tExplosion = { "/resources/images/explosion/explosion_01.png",
+			"/resources/images/explosion/explosion_02.png", "/resources/images/explosion/explosion_03.png",
+			"/resources/images/explosion/explosion_04.png", "/resources/images/explosion/explosion_05.png",
+			"/resources/images/explosion/explosion_06.png", "/resources/images/explosion/explosion_07.png",
+			"/resources/images/explosion/explosion_08.png", };
 
-	private static String[] tRecateur = { "/resource/images/reacteur/reacteur_01.png",
-			"/resource/images/reacteur/reacteur_02.png" };
+	private static String[] tRecateur = { "/resources/images/reacteur/reacteur_01.png",
+			"/resources/images/reacteur/reacteur_02.png" };
 }
