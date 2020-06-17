60c573bf070add42ecc5b1e76a30ca524a85c26d
Wei <zhuw17@hotmail.com>
2020-06-18 00:05:01 +0200

    commit pour le controller, maintenant le vaisseau peut bouger en diagonale, et il ne sort pas du cadre du jeux. Petit bug: il peut descend dans le clavier

--- src/com/avion/controleur/Controller.java
+++ src/com/avion/controleur/Controller.java
@@ -1,14 +1,17 @@
 package com.avion.controleur;
 
+import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.image.BufferedImage;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
 import com.avion.constante.AnimatedPictures;
 import com.avion.constante.Constante;
+import com.avion.constante.Constante.DIRECTION;
+import com.avion.constante.Constante.HORIZONTAL;
+import com.avion.constante.Constante.VERTICAL;
 import com.avion.model.Spacecraft;
 import com.avion.outils.Outils;
 import com.avion.view.GuiClavier;
@@ -17,6 +20,9 @@ public class Controller implements KeyListener {
 
 	private GuiClavier clavier;
 	private Spacecraft vaisseau;
+	DIRECTION dir;
+	HORIZONTAL hor;
+	VERTICAL ver;
 
 	public Controller(GuiClavier pClavier, Spacecraft pVaisseau) {
 		this.clavier = pClavier;
@@ -33,30 +39,61 @@ public class Controller implements KeyListener {
 		switch (e.getKeyCode()) {
 		case KeyEvent.VK_UP:
 			affichageFleche(1);
-			moveVaisseauUp();
+//			moveVaisseauUp();
+			ver = VERTICAL.UP;
 			break;
 
 		case KeyEvent.VK_DOWN:
 			affichageFleche(2);
-			moveVaisseauDown();
+//			moveVaisseauDown();
+			ver = VERTICAL.DOWN;
 			break;
 
 		case KeyEvent.VK_LEFT:
 			affichageFleche(0);
-			moveVaisseauLeft();
+//			moveVaisseauLeft();
 			changeImageDirection(1);
+			hor = HORIZONTAL.LEFT;
 			break;
 
 		case KeyEvent.VK_RIGHT:
 			affichageFleche(3);
-			moveVaisseauRight();
+//			moveVaisseauRight();
 			changeImageDirection(2);
+			hor = HORIZONTAL.RIGHT;
 			break;
 
 		default:
 			break;
 		}
 
+		if (ver == null && hor == HORIZONTAL.RIGHT) {
+			dir = DIRECTION.RIGHT;
+//			moveVaisseauRight();
+		} else if (ver == null && hor == HORIZONTAL.LEFT) {
+			dir = DIRECTION.LEFT;
+//			moveVaisseauLeft();
+		} else if (hor == null && ver == VERTICAL.UP) {
+			dir = DIRECTION.UP;
+//			moveVaisseauUp();
+		} else if (hor == null && ver == VERTICAL.DOWN) {
+			dir = DIRECTION.DOWN;
+//			moveVaisseauDown();
+		} else if (hor == HORIZONTAL.RIGHT && ver == VERTICAL.UP) {
+			dir = DIRECTION.UP_RIGHT;
+//			moveVaisseauUpRight();
+		} else if (hor == HORIZONTAL.RIGHT && ver == VERTICAL.DOWN) {
+			dir = DIRECTION.DOWN_RIGHT;
+//			moveVaisseauDownRight();
+		} else if (hor == HORIZONTAL.LEFT && ver == VERTICAL.UP) {
+			dir = DIRECTION.UP_LEFT;
+//			moveVaisseauUpLeft();
+		} else if (hor == HORIZONTAL.LEFT && ver == VERTICAL.DOWN) {
+			dir = DIRECTION.DOWN_LEFT;
+//			moveVaisseauDownLeft();
+		}
+
+		move(dir);
 	}
 
 	@Override
@@ -64,20 +101,24 @@ public class Controller implements KeyListener {
 		switch (e.getKeyCode()) {
 		case KeyEvent.VK_UP:
 			upReleased(1);
+			ver = null;
 			break;
 
 		case KeyEvent.VK_DOWN:
 			upReleased(2);
+			ver = null;
 			break;
 
 		case KeyEvent.VK_LEFT:
 			upReleased(0);
 			resetImageDirection();
+			hor = null;
 			break;
 
 		case KeyEvent.VK_RIGHT:
 			upReleased(3);
 			resetImageDirection();
+			hor = null;
 			break;
 
 		default:
@@ -99,43 +140,96 @@ public class Controller implements KeyListener {
 		clavier.getJpanelOff(pIndex).revalidate();
 		switch (pIndex) {
 		case 0:
-			clavier.getJpanelOff(pIndex)
-					.add(new JLabel(Outils.imageRotation((BufferedImage) clavier.getFlecheOff(), 0.0)));
+			clavier.getJpanelOff(pIndex).add(new JLabel(Outils.imageRotation(clavier.getFlecheOff(), 0.0)));
 			break;
 
 		case 1:
-			clavier.getJpanelOff(pIndex)
-					.add(new JLabel(Outils.imageRotation((BufferedImage) clavier.getFlecheOff(), 90.0)));
+			clavier.getJpanelOff(pIndex).add(new JLabel(Outils.imageRotation(clavier.getFlecheOff(), 90.0)));
 			break;
 
 		case 2:
-			clavier.getJpanelOff(pIndex)
-					.add(new JLabel(Outils.imageRotation((BufferedImage) clavier.getFlecheOff(), 270.0)));
+			clavier.getJpanelOff(pIndex).add(new JLabel(Outils.imageRotation(clavier.getFlecheOff(), 270.0)));
 			break;
 
 		case 3:
-			clavier.getJpanelOff(pIndex)
-					.add(new JLabel(Outils.imageRotation((BufferedImage) clavier.getFlecheOff(), 180.0)));
+			clavier.getJpanelOff(pIndex).add(new JLabel(Outils.imageRotation(clavier.getFlecheOff(), 180.0)));
 			break;
 
 		}
 		clavier.getJpanelOff(pIndex).repaint();
 	}
 
-	private void moveVaisseauUp() {
-		vaisseau.setLocation(vaisseau.getX(), vaisseau.getY() - Constante.VAISSEAU_SPEED);
-	}
+//	private void moveVaisseauUp() {
+//		vaisseau.setLocation(vaisseau.getX(), vaisseau.getY() - Constante.VAISSEAU_SPEED);
+//	}
+//
+//	private void moveVaisseauDown() {
+//		vaisseau.setLocation(vaisseau.getX(), vaisseau.getY() + Constante.VAISSEAU_SPEED);
+//	}
+//
+//	private void moveVaisseauLeft() {
+//		vaisseau.setLocation(vaisseau.getX() - Constante.VAISSEAU_SPEED, vaisseau.getY());
+//	}
+//
+//	private void moveVaisseauRight() {
+//		vaisseau.setLocation(vaisseau.getX() + Constante.VAISSEAU_SPEED, vaisseau.getY());
+//	}
+//
+//	private void moveVaisseauUpRight() {
+//		vaisseau.setLocation(vaisseau.getX() + Constante.VAISSEAU_SPEED, vaisseau.getY() - Constante.VAISSEAU_SPEED);
+//	}
+//
+//	private void moveVaisseauUpLeft() {
+//		vaisseau.setLocation(vaisseau.getX() - Constante.VAISSEAU_SPEED, vaisseau.getY() - Constante.VAISSEAU_SPEED);
+//	}
+//
+//	private void moveVaisseauDownRight() {
+//		vaisseau.setLocation(vaisseau.getX() + Constante.VAISSEAU_SPEED, vaisseau.getY() + Constante.VAISSEAU_SPEED);
+//	}
+//
+//	private void moveVaisseauDownLeft() {
+//		vaisseau.setLocation(vaisseau.getX() - Constante.VAISSEAU_SPEED, vaisseau.getY() + Constante.VAISSEAU_SPEED);
+//	}
 
-	private void moveVaisseauDown() {
-		vaisseau.setLocation(vaisseau.getX(), vaisseau.getY() + Constante.VAISSEAU_SPEED);
-	}
+	private void move(DIRECTION dir) {
+		Point pos = vaisseau.getLocation();
 
-	private void moveVaisseauLeft() {
-		vaisseau.setLocation(vaisseau.getX() - Constante.VAISSEAU_SPEED, vaisseau.getY());
-	}
+		switch (dir) {
+		case UP:
+			pos.y -= Constante.VAISSEAU_SPEED;
+			break;
+		case RIGHT:
+			pos.x += Constante.VAISSEAU_SPEED;
+			break;
+		case DOWN:
+			pos.y += Constante.VAISSEAU_SPEED;
+			break;
+		case LEFT:
+			pos.x -= Constante.VAISSEAU_SPEED;
+			break;
+		case UP_RIGHT:
+			pos.x += Constante.VAISSEAU_SPEED;
+			pos.y -= Constante.VAISSEAU_SPEED;
+			break;
+		case DOWN_RIGHT:
+			pos.x += Constante.VAISSEAU_SPEED;
+			pos.y += Constante.VAISSEAU_SPEED;
+			break;
+		case DOWN_LEFT:
+			pos.x -= Constante.VAISSEAU_SPEED;
+			pos.y += Constante.VAISSEAU_SPEED;
+			break;
+		case UP_LEFT:
+			pos.x -= Constante.VAISSEAU_SPEED;
+			pos.y -= Constante.VAISSEAU_SPEED;
+			break;
+		}
+		if (pos.x >= Constante.WIDTH - vaisseau.getWidth() || pos.x < 0
+				|| pos.y >= Constante.HEIGHT - vaisseau.getHeight() - Constante.HEIGHT_CLAVIER || pos.y < 0)
+			return;
 
-	private void moveVaisseauRight() {
-		vaisseau.setLocation(vaisseau.getX() + Constante.VAISSEAU_SPEED, vaisseau.getY());
+		vaisseau.setLocation(pos);
+		vaisseau.validate();
 	}
 
 	private void changeImageDirection(int pIndex) {
