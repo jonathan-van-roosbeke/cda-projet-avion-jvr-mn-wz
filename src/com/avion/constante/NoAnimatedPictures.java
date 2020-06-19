8435f88fc9d652f8b41127b85059df9285714e45
Van Roosbeke <jonathan.van.roosbeke@gmail.com>
2020-06-19 09:15:00 +0200

    Correction barre de vie

--- src/com/avion/constante/NoAnimatedPictures.java
+++ src/com/avion/constante/NoAnimatedPictures.java
@@ -8,11 +8,11 @@ public enum NoAnimatedPictures {
 	METEOR_ICEBERG("resources/images/meteor/meteor_iceberg.png"),
 	METEOR_ZIGZAG("resources/images/meteor/meteor_zigzag.png"), FLECHE_OFF("resources/images/fleche/fleche_off.jpg"),
 	FLECHE_ON("resources/images/fleche/fleche_on.jpg"), VIE_01_OFF("resources/images/vie/01_vie_off.png"),
-	VIE_01_ON("resources/images/vie/01_vie_off.png"), VIE_02_OFF("resources/images/vie/02_vie_off.png"),
-	VIE_02_ON("resources/images/vie/02_vie_off.png"), VIE_03_OFF("resources/images/vie/03_vie_off.png"),
-	VIE_03_ON("resources/images/vie/03_vie_off.png"), VIE_04_OFF("resources/images/vie/04_vie_off.png"),
-	VIE_04_ON("resources/images/vie/04_vie_off.png"), VIE_05_OFF("resources/images/vie/05_vie_off.png"),
-	VIE_05_ON("resources/images/vie/05_vie_off.png");
+	VIE_01_ON("resources/images/vie/01_vie_on.png"), VIE_02_OFF("resources/images/vie/02_vie_off.png"),
+	VIE_02_ON("resources/images/vie/02_vie_on.png"), VIE_03_OFF("resources/images/vie/03_vie_off.png"),
+	VIE_03_ON("resources/images/vie/03_vie_on.png"), VIE_04_OFF("resources/images/vie/04_vie_off.png"),
+	VIE_04_ON("resources/images/vie/04_vie_on.png"), VIE_05_OFF("resources/images/vie/05_vie_off.png"),
+	VIE_05_ON("resources/images/vie/05_vie_on.png");
 
 	private String url;
 
--- src/com/avion/view/LifeBar.java
+++ src/com/avion/view/LifeBar.java
@@ -8,6 +8,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
+import com.avion.constante.NoAnimatedPictures;
 import com.avion.model.MeteoritesMoving;
 
 public class LifeBar extends JPanel {
@@ -24,27 +25,27 @@ public class LifeBar extends JPanel {
 		setBackground(new Color(6, 6, 16));
 
 		l1 = new JLabel();
-		l1.setIcon(new ImageIcon("resources/images/vie/01_vie_on.png"));
+		l1.setIcon(new ImageIcon(NoAnimatedPictures.VIE_01_ON.getUrl()));
 		l1.setHorizontalAlignment(SwingConstants.CENTER);
 		this.add(l1);
 
 		l2 = new JLabel();
-		l2.setIcon(new ImageIcon("resources/images/vie/02_vie_on.png"));
+		l2.setIcon(new ImageIcon(NoAnimatedPictures.VIE_02_ON.getUrl()));
 		l2.setHorizontalAlignment(SwingConstants.CENTER);
 		this.add(l2);
 
 		l3 = new JLabel();
-		l3.setIcon(new ImageIcon("resources/images/vie/03_vie_on.png"));
+		l3.setIcon(new ImageIcon(NoAnimatedPictures.VIE_03_ON.getUrl()));
 		l3.setHorizontalAlignment(SwingConstants.CENTER);
 		this.add(l3);
 
 		l4 = new JLabel();
-		l4.setIcon(new ImageIcon("resources/images/vie/04_vie_on.png"));
+		l4.setIcon(new ImageIcon(NoAnimatedPictures.VIE_04_ON.getUrl()));
 		l4.setHorizontalAlignment(SwingConstants.CENTER);
 		this.add(l4);
 
 		l5 = new JLabel();
-		l5.setIcon(new ImageIcon("resources/images/vie/05_vie_on.png"));
+		l5.setIcon(new ImageIcon(NoAnimatedPictures.VIE_05_ON.getUrl()));
 		l5.setHorizontalAlignment(SwingConstants.CENTER);
 		this.add(l5);
 
@@ -53,32 +54,25 @@ public class LifeBar extends JPanel {
 
 	public static void setLifeBar(int nb) {
 		nb = MeteoritesMoving.getLife();
-		if (nb == 100) {
-			for (int i = 0; i < 5; i++) {
-				JLabel jl = new JLabel();
-				jl.setIcon(new ImageIcon("resources/images/vie/0" + i + "_vie_on.png"));
-				jl.setHorizontalAlignment(SwingConstants.CENTER);
-			}
-		}
 
 		if (nb < 80) {
-			l5.setIcon(new ImageIcon("resources/images/vie/01_vie_off.png"));
+			l5.setIcon(new ImageIcon(NoAnimatedPictures.VIE_05_OFF.getUrl()));
 			l5.setHorizontalAlignment(SwingConstants.CENTER);
 		}
 		if (nb < 60) {
-			l4.setIcon(new ImageIcon("resources/images/vie/01_vie_off.png"));
+			l4.setIcon(new ImageIcon(NoAnimatedPictures.VIE_04_OFF.getUrl()));
 			l4.setHorizontalAlignment(SwingConstants.CENTER);
 		}
 		if (nb < 40) {
-			l3.setIcon(new ImageIcon("resources/images/vie/01_vie_off.png"));
+			l3.setIcon(new ImageIcon(NoAnimatedPictures.VIE_03_OFF.getUrl()));
 			l3.setHorizontalAlignment(SwingConstants.CENTER);
 		}
 		if (nb < 20) {
-			l2.setIcon(new ImageIcon("resources/images/vie/01_vie_off.png"));
+			l2.setIcon(new ImageIcon(NoAnimatedPictures.VIE_02_OFF.getUrl()));
 			l2.setHorizontalAlignment(SwingConstants.CENTER);
 		}
 		if (nb < 5) {
-			l1.setIcon(new ImageIcon("resources/images/vie/01_vie_off.png"));
+			l1.setIcon(new ImageIcon(NoAnimatedPictures.VIE_01_OFF.getUrl()));
 			l1.setHorizontalAlignment(SwingConstants.CENTER);
 		}
 	}
